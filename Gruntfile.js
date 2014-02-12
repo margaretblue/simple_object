699532edcfd3e0634da97947e431849d22120ea0
Margaret Morris <margaretblue@gmail.com>
2014-02-11 23:08:08 -0800

    added jshint, simplemocha tests still fake-passing

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,12 +11,41 @@ module.exports = function(grunt) {
       },
 
       all: { src: ['test/**/*.js'] }
+    },
+    jshint: {
+      //src: ['Gruntfile.js', 'src/app/**/*.js', 'src/config.js', 'tests/app/**/*.js'],
+      src: ['Gruntfile.js', 'test/**/*.js', 'lib/**/*.js'],
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        globals: {
+          require: true,
+          define: true,
+          requirejs: true,
+          describe: true,
+          expect: true,
+          it: true
+        }
+      }
     }
   });
 
   // For this to work, you need to have run `npm install grunt-simple-mocha`
   grunt.loadNpmTasks('grunt-simple-mocha');
 
+   // Load JSHint task
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
   // Add a default task. This is optional, of course :)
-  grunt.registerTask('default', 'simplemocha');
+  grunt.registerTask('default', ['simplemocha', 'jshint']);
+
 };
--- test/card_spec.js
+++ test/card_spec.js
@@ -1,14 +1,16 @@
-var expect = require('chai').expect;
-var Book = require('../lib/card').Card;
-  describe('If I create a card with a suit and rank, that the card has that suit and rank', function() {
+var expect = require('chai').expect,
+  Card = require('../lib/card').Card;
+
+describe('Card object tests', function() {
+    var card;
 
     beforeEach(function() {
       card = new Card('Spades', 3);
     });
-      it('card should have property: suit', function() {
-        expect(card).to.have.property('suit');
+        // it('card should have property: rank', function() {
+        //   expect(card).to.have.property('rank');
+
+    describe('constructor', function() {
       });
-      it('card should have property: rank', function() {
-        expect(card).to.have.property('rank');
-      });
+
   });
